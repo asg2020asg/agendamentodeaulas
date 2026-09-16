c02ee9c833560a167e5eb6a37fcfda2cc020d84b
Arthur Santos Gatti <arthurgatti@unifesspa.edu.br>
2026-09-16 17:29:49 -0300

    Remove chave Pix manual

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const siteConfigPath = path.join(__dirname, 'site-config.json');
 const bookingsPath = path.join(__dirname, 'bookings.json');
 const defaultSiteConfig = {
   phone: '5594981911783',
-  pix: '',
   address: 'Endereço a definir',
   map: '',
   times: ['06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00'],
