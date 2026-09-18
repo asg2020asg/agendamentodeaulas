c048be2962f3fab1abb0b4c36fef0263a0e27b55
Arthur Santos Gatti <arthurgatti@unifesspa.edu.br>
2026-09-17 22:04:11 -0300

    Fix CORS for GitHub Pages frontend

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.use(express.json({
 app.use(express.static(path.join(__dirname)));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-password');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
